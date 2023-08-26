0cfdac56ee6e4d45bf23d6655e96e2826f6cb281
Henrique Navarro Morais <henriquenavarromorais@gmail.com>
2023-08-26 09:28:04 -0300

    tudo funcionando

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -7,6 +7,8 @@ import {
   FaNodeJs,
   FaPhp,
   FaRProject,
+  FaRust,
+  FaSwift,
 } from "react-icons/fa";
 import { FaGolang } from "react-icons/fa6";
 import { SiRuby, SiCplusplus } from "react-icons/si";
@@ -18,62 +20,85 @@ export const NavBar = () => {
         <li>
           <FaHome className={styles.icon} />
         </li>
-        <Link to="/java" className={styles.link}>
-          <li>
-            <SiCplusplus className={styles.icon} />
-          </li>
-        </Link>
-        <Link to="/java" className={styles.link}>
-          <li>
-            C
-            <FaJava className={styles.icon} />
-          </li>
-        </Link>
-        <Link to="/java" className={styles.link}>
-          <li>
-            C#
-            <FaJava className={styles.icon} />
-          </li>
-        </Link>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/cpp" className={styles.link}>
+        <li>
+          <SiCplusplus className={styles.icon} />
+        </li>
+      </Link>
+
+      <Link to="/c" className={styles.link}>
+        <li>
+          C
+          <FaJava className={styles.icon} />
+        </li>
+      </Link>
+
+      <Link to="/cs" className={styles.link}>
+        <li>
+          C#
+          <FaJava className={styles.icon} />
+        </li>
+      </Link>
+
+      <Link to="/go" className={styles.link}>
         <li>
           <FaGolang className={styles.icon} />
         </li>
       </Link>
+
       <Link to="/java" className={styles.link}>
         <li>
           <FaJava className={styles.icon} />
         </li>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/kotlin" className={styles.link}>
         <li>
           <TbBrandKotlin className={styles.icon} />
         </li>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/nodejs" className={styles.link}>
         <li>
           <FaNodeJs className={styles.icon} />
         </li>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/php" className={styles.link}>
         <li>
           <FaPhp className={styles.icon} />
         </li>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/pthon" className={styles.link}>
         <li>
           <FaPython className={styles.icon} />
         </li>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/r" className={styles.link}>
+        <li>
+          <FaRProject className={styles.icon} />
+        </li>
+      </Link>
+
+      <Link to="/ruby" className={styles.link}>
         <li>
           <SiRuby className={styles.icon} />
         </li>
       </Link>
-      <Link to="/java" className={styles.link}>
+
+      <Link to="/rust" className={styles.link}>
         <li>
-          <FaRProject className={styles.icon} />
+          <FaRust className={styles.icon} />
+        </li>
+      </Link>
+
+      <Link to="/swift" className={styles.link}>
+        <li>
+          <FaSwift className={styles.icon} />
         </li>
       </Link>
     </ul>
--- frontend/src/components/layout/TextArea.tsx
+++ frontend/src/components/layout/TextArea.tsx
@@ -1,10 +1,42 @@
 import styles from "../../assets/css/TextArea.module.css";
 import { useState } from "react";
 
-function submit(code: any, input: any, output: any) {
+function clear(setOutput: any) {
+  setOutput("");
+}
+
+function submit(code: any, input: any, output: any, setOutput: any) {
+  // Obtém a URL atual do navegador
+  const currentURL = window.location.href;
+  // Divide a URL em partes usando a barra como separador
+  const parts = currentURL.split("/");
+  // Pega a última parte da URL após a última barra
+  const lastPart = parts[parts.length - 1];
+  // Agora você pode usar a última parte como parte da URL na requisição Fetch
+  const url = `http://localhost:8080/${lastPart}`;
+
   console.log(code);
   console.log(input);
   console.log(output);
+  console.log(lastPart);
+  console.log(url);
+
+  setOutput("Compilando...");
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: code,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log("Resposta do servidor:", data);
+      setOutput(data);
+    })
+    .catch((error) => {
+      console.error("Ocorreu um erro:", error);
+    });
 }
 export const TextArea = () => {
   const [code, setCode] = useState("");
@@ -16,12 +48,18 @@ export const TextArea = () => {
       {/*CODE*/}
       <div className={styles.container_left}>
         <div className={styles.header_container}>
-          <div className={styles.align_left}>main.c</div>
+          <div className={styles.align_left}>
+            <input
+              type="submit"
+              value="main.c"
+              className={styles.button_tittle}
+            />
+          </div>
           <div className={styles.align_right}>
             <input
-              onClick={() => submit(code, input, output)}
+              onClick={() => submit(code, input, output, setOutput)}
               type="submit"
-              value="run"
+              value="Run"
               className={styles.button}
             />
           </div>
@@ -38,10 +76,19 @@ export const TextArea = () => {
       <div className={styles.container_right}>
         <div className={styles.header_container}>
           <div className={styles.align_left}>
-            <span>Output</span>
+            <input
+              type="submit"
+              value="Output"
+              className={styles.button_tittle}
+            />
           </div>
           <div className={styles.align_right}>
-            <input type="submit" value="clear" className={styles.button} />
+            <input
+              type="submit"
+              value="Clear"
+              className={styles.button}
+              onClick={() => clear(setOutput)}
+            />
           </div>
         </div>
         <textarea
